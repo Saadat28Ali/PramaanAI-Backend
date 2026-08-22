b42dc95cfdfae922b4a56831dbc7f2ea9cbdc424
Saadat Ali <ali.saadat.1003@gmail.com>
2026-08-22 10:05:19 +0530

    Added a basic file upload path "/ocr"

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
-from flask import Flask;
+from flask import Flask, request;
+
 app = Flask(__name__);
 
-@app.route('/')
+@app.route('/', methods=["GET"])
 def hello_world():
 	return "Hello world";
+
+@app.route("/ocr", methods=["GET", "POST"])
+def ocr_upload():
+	if request.method == "POST":
+		request.files["image"].save("./ocr_uploads/new.png");
+		return "File uploaded.";
+	else:
+		return "Retry with POST method.";
