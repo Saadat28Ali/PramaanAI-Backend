ede8f84cdc1417b3d8341fa094c1a2df732da2a9
Saadat Ali <ali.saadat.1003@gmail.com>
2026-08-22 10:38:33 +0530

    modified file upload mechanism to name files by time and date

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, request;
+from os import path, mkdir
+from time import time, gmtime
 
 app = Flask(__name__);
 
@@ -9,7 +11,24 @@ def hello_world():
 @app.route("/ocr", methods=["GET", "POST"])
 def ocr_upload():
 	if request.method == "POST":
-		request.files["image"].save("./ocr_uploads/new.png");
+
+		# trying to make the ./ocr directory
+		# if it already exists, this part is skipped
+
+		try:
+			mkdir(path.abspath("./ocr"));
+		except FileExistsError:
+			pass;
+
+		# saving the received file in ./ocr
+		# the filename is based on time of upload
+
+		current_time = gmtime(time());
+		with open(path.abspath(f"./ocr/{current_time.tm_hour}-{current_time.tm_min}-{current_time.tm_sec} {current_time.tm_mday}-{current_time.tm_mon}-{current_time.tm_year}.png"), "wb") as fh:
+			request.files["image"].save(fh);
+
+		# returning response
+
 		return "File uploaded.";
 	else:
 		return "Retry with POST method.";
